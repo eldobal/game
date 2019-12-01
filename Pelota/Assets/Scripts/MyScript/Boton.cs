859a308781fb94202613ca0a452a91a0848cb34d
sebastian baldovinos <seba_baldo98@live.cl>
2019-12-01 14:37:27 +0100

    Menu
    creacion de un menu basico

---
+++ Pelota/Assets/Scripts/MyScript/Boton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boton : MonoBehaviour
+{
+
+    public void PlayScene() {
+        SceneManager.LoadScene("Menu 3D");
+    }
+
+    public void Quitgame() {
+        Application.Quit();
+    }
+}
